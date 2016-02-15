1b2f8ef1f836a89a2e0926e2ef65319d9c88552a
Victor Cotrim <cotrim149@gmail.com>
2016-02-15 00:15:14 -0200

    Draw table changing border color of UILabel #1

--- knapsack/ViewController.swift
+++ knapsack/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		self.drawTable()
+		self.drawTable(lines: 5, columns: 3)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -21,18 +21,22 @@ class ViewController: UIViewController {
 	}
 
 	
-	func drawTable(){
+	func drawTable(lines lines:Int, columns:Int){
 		var x = CGFloat(0)
 		var y = CGFloat(0)
 		let width = CGFloat(35)
 		let heigth = CGFloat(20)
-		for(var line = 0; line < 10; line++){
-			for(var column = 0; column < 10; column++){
+		for(var line = 0; line < lines; line++){
+			for(var column = 0; column < columns; column++){
 				let labelSize = CGRectMake(x, y, width, heigth)
 				x += width
 				let label = UILabel(frame: labelSize)
+				label.layer.borderColor = UIColor.blackColor().CGColor
 				label.text = String(line) + String(column)
+				label.layer.borderWidth = 1
+				label.textAlignment = NSTextAlignment.Center
 				self.view.addSubview(label)
+				
 			}
 			x = CGFloat(0)
 			y += heigth
